9cc7a66a2b8cecd25129a93b0965ea02d2eff07c
Oscar <osalvador@gmail.com>
2015-09-16 13:58:15 +0200

    Create installation script

---
+++ tapigen2_install.sql
@@ -0,0 +1,22 @@
+Rem    NAME
+Rem      tapigen2_install.sql
+Rem
+Rem    DESCRIPTION
+Rem 	 TAPI Gen2 installation script.
+Rem
+Rem    REQUIREMENTS
+Rem      - Oracle Database 10 or later
+Rem
+Rem    Example:
+Rem      sqlplus "user/userpasss" @tapigen2_install
+Rem
+Rem    MODIFIED   (MM/DD/YYYY)
+Rem    osalvador  16/09/2015 - Created
+
+@@tePLSQL.pks
+@@TAPI_GEN2.pks
+@@tePLSQL.pkb
+@@TAPI_GEN2.pkb
+
+quit;
+/
